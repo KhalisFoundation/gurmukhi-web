de06b016a83cb9ebd0d6d1644b718eedb2071fc8
Amitoj Singh <amitojsingh95@gmail.com>
2023-12-16 11:44:40 -0700

    feat: meta data and favicon

--- src/pages/word/layout.tsx
+++ src/pages/word/layout.tsx
@@ -1,10 +1,14 @@
+import Meta from 'components/meta';
+import metaTags from 'constants/meta';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 export default function WordsPageLayout() {
-  // check if children contains 
+  // check if children contains
+  const { title, description } = metaTags.WORD;
   return (
-    <div className="flex flex-col items-center justify-between gap-5 p-12 pb-0">
+    <div className='flex flex-col items-center justify-between gap-5 p-12 pb-0'>
+      <Meta title={title} description={description} />
       <Outlet />
     </div>
   );
