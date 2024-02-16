3c3e27c3fc44853b030c36f035cccfeddf4326de
Amitoj Singh <amitojsingh95@gmail.com>
2024-02-16 16:21:04 -0700

    fix: deploy

--- src/pages/dashboard/utils/index.ts
+++ src/pages/dashboard/utils/index.ts
@@ -45,13 +45,3 @@ export const shuffleArray = (array: GameScreen[]) => {
   }
   return array;
 };
-export const shuffleArray = (array: GameScreen[]) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    // Generate a random index between 0 and i
-    const j = Math.floor(Math.random() * (i + 1));
-
-    // Swap elements at indices i and j
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-};
