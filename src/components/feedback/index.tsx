fc5600ce41008740fb6064a67fed9ed30ceb52f2
Gurmehar Singh <gsingh@waheguroo.com>
2024-02-17 12:35:01 +1100

    initial commit for feedback form

--- src/components/feedback/index.tsx
+++ src/components/feedback/index.tsx
@@ -3,6 +3,7 @@ import { useUserAuth } from 'auth';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router';
 import { SignError } from 'types';
+import InputWithIcon from 'components/input/InputWithIcon';
 
 function FeedbackForm() {
     const { t: text } = useTranslation();
@@ -18,8 +19,26 @@ function FeedbackForm() {
       className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
       action='https://submit.jotform.com/submit/240394066555864'
     >
+      <InputWithIcon
+        id="input_11"
+        name="q11_name"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder={text('NAME')}
+        icon="user"
+      />
+      <InputWithIcon
+        id="input_13"
+        name="q13_email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder={text('EMAIL')}
+        icon="email"
+      />
     </form>
   )
 }
 
-export default FeedbackForm
+export default FeedbackForm;
