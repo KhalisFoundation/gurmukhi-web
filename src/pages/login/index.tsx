df5c002806ba63c23a8c95c41d0096aec430c08a
Amitoj Singh <amitojsingh95@gmail.com>
2024-02-22 16:52:47 -0700

    fix: sign In with google button

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -8,7 +8,7 @@ export default function Login() {
   const { title, description } = metaTags.LOGIN;
 
   return (
-    <section className='flex flex-row w-full items-center justify-evenly m-auto my-0 h-5/6'>
+    <section className='flex flex-row w-full items-center justify-evenly m-auto my-0 h-full'>
       <Meta title={title} description={description} />
       <Gurfateh />
       <SignIn />
