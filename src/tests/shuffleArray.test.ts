412bbb663d75d4ee20186ed54620dd4993c874ba
Gurmehar Singh <gsingh@waheguroo.com>
2024-04-22 17:27:34 +1000

    fix: eslint fixes

--- src/tests/shuffleArray.test.ts
+++ src/tests/shuffleArray.test.ts
@@ -1,20 +1,39 @@
 import { shuffleArray } from '../pages/dashboard/utils/helpers';
+import CONSTANTS from 'constants/constant';
 
 describe('shuffleArray', () => {
   it('keeps the array the same length after shuffling', () => {
-    const array = [1, 2, 3, 4, 5];
+    const array = [
+      CONSTANTS.DEFAULT_ONE,
+      CONSTANTS.DEFAULT_TWO,
+      CONSTANTS.DEFAULT_THREE,
+      CONSTANTS.DEFAULT_FOUR,
+      CONSTANTS.DEFAULT_FIVE,
+    ];
     const shuffledArray = shuffleArray([...array]);
     expect(shuffledArray.length).toBe(array.length);
   });
 
   it('contains the same elements before and after shuffling', () => {
-    const array = [1, 2, 3, 4, 5];
+    const array = [
+      CONSTANTS.DEFAULT_ONE,
+      CONSTANTS.DEFAULT_TWO,
+      CONSTANTS.DEFAULT_THREE,
+      CONSTANTS.DEFAULT_FOUR,
+      CONSTANTS.DEFAULT_FIVE,
+    ];
     const shuffledArray = shuffleArray([...array]);
     expect(shuffledArray.sort()).toEqual(array.sort());
   });
 
   it('changes the order of elements (run multiple times to check for randomness)', () => {
-    const array = [1, 2, 3, 4, 5];
+    const array = [
+      CONSTANTS.DEFAULT_ONE,
+      CONSTANTS.DEFAULT_TWO,
+      CONSTANTS.DEFAULT_THREE,
+      CONSTANTS.DEFAULT_FOUR,
+      CONSTANTS.DEFAULT_FIVE,
+    ];
     let differentOrderCount = 0;
     const iterations = 10; // Run shuffle multiple times
 
