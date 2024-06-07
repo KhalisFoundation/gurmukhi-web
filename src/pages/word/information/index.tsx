c9c13350f86ebff209e54b61f5454404605cf66a
Gurmehar Singh <103510447@student.swin.edu.au>
2024-06-07 11:01:08 +1000

    fix: optimised random word box

--- src/pages/word/information/index.tsx
+++ src/pages/word/information/index.tsx
@@ -158,58 +158,52 @@ export default function Information() {
                 })}
             </div>
             <div className='flex items-center justify-around gap-5 w-full h-1/2'>
-              <div
-                className={`w-5/6 h-full cardImage bg-cover bg-sky-100 bg-blend-soft-light hover:bg-sky-50 border-2 border-sky-200 shadow-lg rounded-lg lg:w-2/5  ${
-                  currentWord?.synonyms && currentWord.synonyms.length === 0 ? 'hidden' : ''
-                }`}
-              >
-                <h2 className='text-black tracking-widest ms-2 my-2'>
-                  {text('SYNONYMS').toUpperCase()}
-                </h2>
-                <div className='w-11/12 flex flex-col gap-2 m-auto'>
-                  {currentWord?.synonyms &&
-                    currentWord.synonyms.map((word: MiniWord | string) => {
-                      if (typeof word !== 'string') {
-                        return (
-                          <div
-                            key={word.id}
-                            className={
-                              'flex h-min w-full p-4 text-black text-sm rounded-lg z-10 bg-white'
-                            }
-                          >
-                            {word.word} ({convertToTitleCase(word.translation ?? '')})
-                          </div>
-                        );
-                      }
-                    })}
-                </div>
-              </div>
-              <div
-                className={` w-5/6 h-full cardImage bg-cover bg-sky-100 bg-blend-soft-light hover:bg-sky-50 border-2 border-sky-200 shadow-lg rounded-lg lg:w-2/5 ${
-                  currentWord?.antonyms && currentWord.antonyms.length === 0 ? 'hidden' : ''
-                }`}
-              >
-                <h2 className='text-black tracking-widest ms-2 my-2'>
-                  {text('ANTONYMS').toUpperCase()}
-                </h2>
-                <div className='w-11/12 flex flex-col gap-2 m-auto'>
-                  {currentWord?.antonyms &&
-                    currentWord.antonyms.map((word: MiniWord | string) => {
-                      if (typeof word !== 'string') {
-                        return (
-                          <div
-                            key={word.id}
-                            className={
-                              'flex h-min w-full p-4 text-black text-sm rounded-lg z-10 bg-white'
-                            }
-                          >
-                            {word.word} ({convertToTitleCase(word.translation ?? '')})
-                          </div>
-                        );
-                      }
-                    })}
-                </div>
-              </div>
+              {currentWord?.synonyms && currentWord.synonyms.length === 0 && (
+                <div className={'w-5/6 h-full cardImage bg-cover bg-sky-100 bg-blend-soft-light hover:bg-sky-50 border-2 border-sky-200 shadow-lg rounded-lg lg:w-2/5'}>
+                  <h2 className='text-black tracking-widest ms-2 my-2'>
+                    {text('SYNONYMS').toUpperCase()}
+                  </h2>
+                  <div className='w-11/12 flex flex-col gap-2 m-auto'>
+                    {currentWord?.synonyms &&
+                      currentWord.synonyms.map((word: MiniWord | string) => {
+                        if (typeof word !== 'string') {
+                          return (
+                            <div
+                              key={word.id}
+                              className={
+                                'flex h-min w-full p-4 text-black text-sm rounded-lg z-10 bg-white'
+                              }
+                            >
+                              {word.word} ({convertToTitleCase(word.translation ?? '')})
+                            </div>
+                          );
+                        }
+                      })}
+                  </div>
+                </div>)}
+              {currentWord?.antonyms && currentWord.antonyms.length === 0 && (
+                <div className={'w-5/6 h-full cardImage bg-cover bg-sky-100 bg-blend-soft-light hover:bg-sky-50 border-2 border-sky-200 shadow-lg rounded-lg lg:w-2/5'}>
+                  <h2 className='text-black tracking-widest ms-2 my-2'>
+                    {text('ANTONYMS').toUpperCase()}
+                  </h2>
+                  <div className='w-11/12 flex flex-col gap-2 m-auto'>
+                    {currentWord?.antonyms &&
+                      currentWord.antonyms.map((word: MiniWord | string) => {
+                        if (typeof word !== 'string') {
+                          return (
+                            <div
+                              key={word.id}
+                              className={
+                                'flex h-min w-full p-4 text-black text-sm rounded-lg z-10 bg-white'
+                              }
+                            >
+                              {word.word} ({convertToTitleCase(word.translation ?? '')})
+                            </div>
+                          );
+                        }
+                      })}
+                  </div>
+                </div>)}
             </div>
           </div>
         </div>
