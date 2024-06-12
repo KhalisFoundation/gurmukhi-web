4d3bb55162c97803f5c95dd89937ee7200a2f566
Amitoj Singh <amitojsingh95@gmail.com>
2024-06-11 21:14:52 -0600

    fix: added word

--- src/pages/word/semantics/index.tsx
+++ src/pages/word/semantics/index.tsx
@@ -1,15 +1,14 @@
-/* eslint-disable indent */
-/* eslint-disable @typescript-eslint/indent */
 import React, { useEffect, useRef, useState } from 'react';
 import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd';
+import { useLocation } from 'react-router-dom';
 import Confetti from 'react-confetti';
 import { useTranslation } from 'react-i18next';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import BackBtn from 'components/buttons/BackBtn';
 import LevelsFooter from 'components/levels-footer/LevelsFooter';
 import Meta from 'components/meta';
 import metaTags from 'constants/meta';
+import { getWordById } from 'database/default';
 import { showToastMessage, createSemanticDraggables } from 'utils';
 import { processWords, semanticsOnDrag } from './hooks';
 import ALL_CONSTANT from 'constants/constant';
@@ -19,57 +18,24 @@ import CONSTANTS from 'constants/constant';
 
 export default function Semantics() {
   const { t: text } = useTranslation();
+  const location = useLocation();
   const { title, description } = metaTags.SEMANTICS;
-  // Use useLocation to get the search parameters from the URL
-  const currentGamePosition = useAppSelector((state) => state.currentGamePosition);
-  const currentLevel = useAppSelector((state) => state.currentLevel);
-  // const location = useLocation();
-  const [showConfetti, setShowConfetti] = useState(false);
-
-  // Extract the "id" parameter from the search string in the URL
-  // const searchParams = new URLSearchParams(location.search);
-  // const wordId = searchParams.get('id') ?? '';
-  const synonymsText = text('SYNONYMS');
-  const antonymsText = text('ANTONYMS');
-
-  // Create a ref to store the reference to the HTML element
-  const jumpBoxRef = useRef<any>(null);
-
-  // fetch word from state using wordId
-  // const currentWord = useMemo(() => wordData.find((word) => word.id === wordId) ?? {}, [wordId]);
-  const currentWord: WordType = {
-    antonyms: [],
-    audioURL:
-      'https://storage.googleapis.com/gurmukhi-dev.appspot.com/words/%E0%A8%B9%E0%A8%B5%E0%A8%BE-1JUvhEQWP1ed01JeQRHT.mp3',
-    created_at: {
-      seconds: 1711740722,
-      nanoseconds: 15000000,
-    },
-    created_by: 'kpravleen8@gmail.com',
-    images: [
-      'https://images.pexels.com/photos/54300/pexels-photo-54300.jpeg?auto=compress&cs=tinysrgb&w=600',
-    ],
-    is_for_support: false,
-    meaning_english: 'Necessary for breathing and part of the wind cycle.',
-    meaning_punjabi: 'ਸਾਹ ਲੈਣ ਲਈ ਅਤੇ ਪਵਨ ਚੱਕਰ ਦਾ ਹਿੱਸਾ।',
-    notes: '',
-    part_of_speech: 'noun',
-    status: 'feedback-punjabi',
-    synonyms: [],
-    translation: 'wind, the air',
-    updated_at: {
-      seconds: 1711740722,
-      nanoseconds: 15000000,
-    },
-    updated_by: 'letslearnpanjabi@gmail.com',
-    word: 'ਹਵਾ',
-    id: '1JUvhEQWP1ed01JeQRHT',
-  };
+  const [wordID, setWordID] = useState<string | null>(null);
+  const [isLoading, toggleLoading] = useState<boolean | null>(null);
   const [words, setWords] = useState<WordType[]>([]);
+  const [currentWord, setCurrentWord] = useState<WordType | null>(null);
   const [synonyms, setSynonyms] = useState<WordType[]>([]);
   const [isSynonymsDisabled, setIsSynonymsDisabled] = useState<boolean>(false);
   const [antonyms, setAntonyms] = useState<WordType[]>([]);
   const [isAntonymsDisabled, setIsAntonymsDisabled] = useState<boolean>(false);
+  const currentGamePosition = useAppSelector((state) => state.currentGamePosition);
+  const currentLevel = useAppSelector((state) => state.currentLevel);
+  const [showConfetti, setShowConfetti] = useState(false);
+  const synonymsText = text('SYNONYMS');
+  const antonymsText = text('ANTONYMS');
+  // Create a ref to store the reference to the HTML element
+  const jumpBoxRef = useRef<any>(null);
+  // fetch word from state using wordId
 
   const onDragEnd = (result: DropResult) => {
     const { source, destination } = result;
@@ -85,15 +51,12 @@ export default function Semantics() {
       setAntonyms,
     );
   };
-
-  useEffect(() => {
-    // updateSemanticWordsData(wordId, currentWord, wordData, setWords);
-  }, []);
-
   useEffect(() => {
     const synonymIds = new Set(synonyms.map((synonym) => synonym.id));
     const antonymIds = new Set(antonyms.map((antonym) => antonym.id));
-
+    if (!currentWord) {
+      return;
+    }
     const synonymFound =
       currentWord.synonyms && currentWord.synonyms.length > 0
         ? currentWord.synonyms.map((synonymId) => {
@@ -113,11 +76,39 @@ export default function Semantics() {
     const allWords = [...synonymFound, ...antonymFound];
     // if all words are found, show confetti
     if (allWords.every((wordFound) => wordFound)) {
-      setShowConfetti(true);
+      // setShowConfetti(true);
     } else {
       setShowConfetti(false);
     }
   }, [currentWord, synonyms, antonyms]);
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    console.log(searchParams.get('id'));
+    setWordID(searchParams.get('id'));
+  }, [location.search]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!wordID) {
+        toggleLoading(false);
+        return;
+      }
+      toggleLoading(true);
+      const wordData = await getWordById(wordID, true);
+      console.log('wordData', wordData);
+      if (wordData !== null) {
+        setCurrentWord(wordData);
+      }
+      toggleLoading(false);
+    };
+
+    if (location.state?.data) {
+      setCurrentWord(location.state.data);
+    } else {
+      fetchData();
+    }
+  }, [wordID]);
 
   useEffect(() => {
     if (showConfetti) {
@@ -129,6 +120,9 @@ export default function Semantics() {
   }, [showConfetti]);
 
   useEffect(() => {
+    if (!currentWord) {
+      return;
+    }
     processWords(
       synonyms,
       text('SYNONYMS'),
@@ -155,15 +149,14 @@ export default function Semantics() {
     );
   }, [synonyms, antonyms, words, currentWord]);
 
-  if (!currentWord.word) {
+  if (!currentWord) {
     // Handle case when word is not found
     return <div>{text('WORD_NOT_FOUND')}</div>;
   }
 
   return (
-    <div className='flex flex-col h-screen items-center justify-between'>
+    <div className='flex flex-col h-full w-full items-center justify-between'>
       <Meta title={title} description={description} />
-      <BackBtn navlink={-1} />
       <DragDropContext onDragEnd={onDragEnd}>
         <div className='flex flex-col h-full justify-center items-center gap-5 pb-12'>
           <h1 className='text-4xl gurmukhi text-black'>{currentWord.word}</h1>
@@ -253,6 +246,7 @@ export default function Semantics() {
         currentLevel={currentLevel}
         currentGamePosition={currentGamePosition}
         isDisabled={false}
+        isLoading={isLoading}
       />
     </div>
   );
