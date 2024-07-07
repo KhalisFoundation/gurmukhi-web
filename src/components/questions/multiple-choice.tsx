a0b0954a46cc3258914164a7c1d4c8adc7155d0c
Gurmehar Singh <gsingh@waheguroo.com>
2024-07-07 14:54:18 +1000

    fix: minor test fixes

--- src/components/questions/multiple-choice.tsx
+++ src/components/questions/multiple-choice.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, useEffect } from 'react';
+import React, { Suspense, lazy, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Option, QuestionData, User } from 'types';
 import OptionBtn from 'components/buttons/Option';
@@ -103,13 +103,15 @@ export default function MultipleChoiceQuestion({
         <h1 className='gurmukhi text-black font-semibold leading-snug md:text-5xl text-xl'>
           {highlightWord(questionData.question, questionData.word)}
         </h1>
-        <TextToSpeechBtn
-          backgroundColor='bg-white-175'
-          text={questionData.question}
-          type={ALL_CONSTANT.QUESTION}
-          audioURL={questionData.audioURL}
-          id={questionData.id}
-        />
+        <Suspense fallback={<div>Loading...</div>}>
+          <TextToSpeechBtn
+            backgroundColor='bg-white-175'
+            text={questionData.question}
+            type={ALL_CONSTANT.QUESTION}
+            audioURL={questionData.audioURL}
+            id={questionData.id}
+          />
+        </Suspense>
       </div>
       {hasImage && (
         <div className='w-full h-full'>
